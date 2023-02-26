643522c23f3b73b54d2e04fc4f42df13d4a97897
galcia <galcia.gs@gmail.com>
2023-02-26 11:19:47 +0900

    オオモノ追加

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
-const KingSalmonid = ['Cohozuna', 'Horrorboros'];
-const KingSalmonidName = { Cohozuna: 'ヨコヅナ', Horrorboros: 'タツ' };
 const LOCAL_STORAGE_KEY = "0b8210cf751a851e31a9985e904b14e3bd4b1cb76cdc4d554879030e9eb557e1fc68c3d9e17b0aea878d300a1918c7c2b0584f03355eb5f5e5f37daf359b1873"
 
-var cnt = { Cohozuna: { count: 0 }, Horrorboros: { count: 0 } };
+const KingSalmonid = ['Cohozuna', 'Horrorboros'];
+const BossSalmonids = ['steelhead', 'Flyfish', 'SteelEel', 'Scrapper', 'Stinger', 'Maws', 'BigShot', 'Drizzler', 'FishStick', 'FlipperFlopper', 'SlamminLid']
+const SalmonidsData =
+{
+  'Cohozuna': { nameJP: 'ヨコヅナ' },
+  'Horrorboros': { nameJP: 'タツ' },
+  'steelhead': { HP: 300, nameJP: 'バクダン' },
+  'Flyfish': { HP: 360, nameJP: 'カタパッド' },
+  'Scrapper': { HP: 400, nameJP: 'テッパン' },
+  'SteelEel': { HP: 500, nameJP: 'ヘビ' },
+  'Stinger': { HP: 60, nameJP: 'タワー' },
+  'Maws': { HP: 1200, nameJP: 'モグラ' },
+  'Drizzler': { HP: 400, nameJP: 'コウモリ' },
+  'FishStick': { HP: 400, nameJP: 'ハシラ' },
+  'FlipperFlopper': { HP: 400, nameJP: 'ダイバー' },
+  'BigShot': { HP: 1200, nameJP: 'テッキュウ' },
+  'SlamminLid': { HP: 400, nameJP: 'ナベブタ' },
+}
+
+var data = {
+  'Cohozuna': { count: 0 }, 'Horrorboros': { count: 0 },
+  'steelhead': { count: 0 }, 'Flyfish': { count: 0 }, 'SteelEel': { count: 0 }, 'Stinger': { count: 0 },
+  'Maws': { count: 0 }, 'Drizzler': { count: 0 }, 'FishStick': { count: 0 }, 'FlipperFlopper': { count: 0 }, 'SlamminLid': { count: 0 }
+};
 
 function init() {
+  $("#log").prop("readonly", true);
   loadLocalStorage()
-  setSubjugation();
-  KingSalmonid.forEach(key => {
-    $(`#${key}_cnt`).text(cnt[key].count)
-  })
+  makeBossSalmonidsTable(BossSalmonids)
+  setSubjugation()
 }
 
 function setSubjugationSum(id, n) {
-  cnt[id].count = n
+
+  console.log(`setSubjugationSum:${id},${n}`)
+  data[id].count = n
   $(`#${id}_cnt`).text(n);
 
 
@@ -22,6 +44,7 @@ function setSubjugationSum(id, n) {
   saveLocalStorage()
 }
 function getSubjugationSum(id) {
+  console.log(`getSubjugationSum:${id}`)
   return Number($(`#${id}_cnt`).text());
 }
 
@@ -30,7 +53,7 @@ function setSubjugation() {
 }
 
 function getSum() {
-  return cnt.Cohozuna.count + cnt.Horrorboros.count
+  return data.Cohozuna.count + data.Horrorboros.count
 }
 
 
@@ -38,7 +61,7 @@ function getSum() {
  * 
  */
 function saveLocalStorage() {
-  var setjson = JSON.stringify(cnt);
+  var setjson = JSON.stringify(data);
   localStorage.setItem(LOCAL_STORAGE_KEY, setjson);
 
   addLog("saved local storage")
@@ -50,7 +73,18 @@ function saveLocalStorage() {
 function loadLocalStorage() {
   if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
     var obj = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    cnt = obj
+    data = obj
+
+    // BossSalmonids.forEach(key => {
+    //   if (!data[key]) {
+    //     data[key] = { count: 0 }
+    //   }
+    // })
+    // KingSalmonid.forEach(key => {
+    //   if (!data[key]) {
+    //     data[key] = { count: 0 }
+    //   }
+    // })
 
     addLog("load local storage")
   } else {
@@ -68,5 +102,36 @@ function deleteLocalStorage() {
 }
 
 function addLog(text) {
-  $('#log').text(text + '\n' + $('#log').text())
+  data['log'] = '(' + getTimeStamp() + '):' + text + '\n' + data['log']
+  $('#log').text(data['log'])
+}
+
+function getTimeStamp() {
+  let date = new Date()
+  return date.toLocaleString("ja", {
+    "year": "numeric",
+    "month": "2-digit",
+    "day": "2-digit",
+    "hour": "2-digit",
+    "minute": "2-digit",
+    "second": "2-digit"
+  }) + '.' + zerofill(date.getMilliseconds(), 3)
+
+  function zerofill(str, digit) {
+    return ('0'.repeat(digit) + str).slice(-digit)
+  }
+}
+
+function makeBossSalmonidsTable() {
+
+  let tablestr = '<table class="kingSalmonid-name"><tr><td>名称</td><td>討伐数</td><td></td></tr>';
+  Object.keys(SalmonidsData).forEach(key => {
+    tablestr += `<tr id="${key}">`
+    tablestr += `<td>${SalmonidsData[key].nameJP}</td>`
+    tablestr += `<td id="${key}_cnt">${data[key].count}</td>`
+    tablestr += `<td><input type="button" value="ー" onclick="setSubjugationSum('${key}',getSubjugationSum('${key}')-1)"> <input type="button" value="＋" onclick="setSubjugationSum('${key}',getSubjugationSum('${key}')+1)"></td>`
+    tablestr += `</tr>`
+  })
+  tablestr += '</table>'
+  $('#BossSalmonids_area').append(tablestr)
 }
